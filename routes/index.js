ec58063af6102c995fa519840e9b098e11639551
DevUser <devuser@CodeLearn-Asia.local>
2018-08-09 13:42:53 +0800

    initial route for get All Pharmacies

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Eddie' });
 });
+
+/* GET ALL PHARMACIES*/
+
+router.get('/api/v1/getAllPharmacies', function(req, res, next) {
+  res.render('index', { title: 'we need to do some work here' });
+});
+
 
 module.exports = router;
